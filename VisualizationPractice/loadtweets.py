8b52fae1c13e874afa2e3d55c41e0ff2f19ed962
Weipu Zhao <weipuz@users.noreply.github.com>
2015-04-13 12:24:00 -0700

    Update loadtweets.py

--- VisualizationPractice/loadtweets.py
+++ VisualizationPractice/loadtweets.py
@@ -3,10 +3,10 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 
 #Variables that contains the user credentials to access Twitter API 
-access_token = "2832187939-ATv9XfdOFKU3NoQScnjxpTAGcwkBnouAN6jIMqJ"
-access_token_secret = "c3fdNVanofyNCBZxoRHk2b8vTSwDq4ET3RNQ0829jcD0U"
-consumer_key = "SC5pkBDv2ExnzC1vDuseiKbJ8"
-consumer_secret = "EbXaH7DIwZUlY9GJpf12hSCTV2oEFG2R53dn2zGQsQqNJtweaT"
+access_token = "#"
+access_token_secret = "#"
+consumer_key = "#"
+consumer_secret = "#"
 
 
 #This is a basic listener that just prints received tweets to stdout.
